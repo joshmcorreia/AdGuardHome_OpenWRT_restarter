513a52765b2c916d1c1d33801a26e3b9e97e325b
Josh Correia <joshmcorreia@gmail.com>
2025-10-04 23:55:19 -0700

    Send discord notifications

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
+use chrono::Local;
 use crossterm::{QueueableCommand, cursor};
+use serenity::builder::ExecuteWebhook;
+use serenity::http::Http;
+use serenity::model::webhook::Webhook;
 use std::io::{Write, stdout};
 use std::process::{Command, Stdio};
 use std::{thread, time};
-use chrono::Local;
 
-fn sleep_30_sec() {
-    let seconds_to_sleep = time::Duration::from_secs(30);
+fn sleep_seconds(num_sec_to_sleep: u64) {
+    let seconds_to_sleep = time::Duration::from_secs(num_sec_to_sleep);
     thread::sleep(seconds_to_sleep);
 }
 
-fn main() {
+async fn send_discord_message(message: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let webhook_url = "REPLACE_ME";
+    let http = Http::new("");
+    let webhook = Webhook::from_url(&http, webhook_url).await?;
+    let builder = ExecuteWebhook::new().content(message).username("JoshBot");
+    webhook.execute(&http, false, builder).await?;
+    Ok(())
+}
+
+fn restart_adguardhome() {
+    let restart_adguardhome_result = Command::new("service")
+        .arg("adguardhome")
+        .arg("restart")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()
+        .expect("Restarting adguardhome");
+    if !restart_adguardhome_result.success() {
+        println!("Failed to restart adguardhome!");
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stdout = stdout();
     let date = Local::now();
-    println!("AdGuardHome OpenWRT Restarter 1.0.0 initialized on {}.", date.format("%m/%d/%Y %H:%M:%S"));
+    println!(
+        "AdGuardHome OpenWRT Restarter 1.0.0 initialized on {}.",
+        date.format("%m/%d/%Y %H:%M:%S")
+    );
     let mut times_checked = 0;
 
     let google_dns_ip_address = "8.8.8.8";
@@ -37,7 +66,9 @@ fn main() {
             .expect("Checking if we have an internet connection");
         if !ping_google_ip_result.success() {
             println!("The internet is down!");
-            sleep_30_sec();
+            // There isn't a way to send the user a discord message since their internet is out,
+            // but we at least log it in the console
+            sleep_seconds(30);
             continue;
         }
 
@@ -51,8 +82,13 @@ fn main() {
             .expect("Checking if DNS is working");
         if !ping_google_com_result.success() {
             println!("DNS appears to be down!");
+
+            restart_adguardhome();
+            // give the service some time to come back online
+            sleep_seconds(10);
+            send_discord_message("@everyone DNS broke so AdGuardHome was restarted").await?;
         }
 
-        sleep_30_sec();
+        sleep_seconds(30);
     }
 }
